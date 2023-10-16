a0c30ca903641836dfbbbe3f93c8181f055fe48e
Crowdbotics <team@crowdbotics.com>
2023-10-16 16:56:52 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "sutherland-gen-ai-b-43971",
+  url: "https://sutherland-gen-ai-b-43971.botics.co",
+  api: "https://sutherland-gen-ai-b-43971.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
